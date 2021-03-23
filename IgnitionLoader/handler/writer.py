626c045885cebdbd690fef77fc4550523be1b082
megamaz <raphael.mazuel@gmail.com>
2021-03-23 16:59:09 -0700

    Started (nonworking) camera data

--- IgnitionLoader/handler/writer.py
+++ IgnitionLoader/handler/writer.py
@@ -1,4 +1,4 @@
-import bpy, os, json
+import bpy, os, json, random
 from bpy_extras.io_utils import ExportHelper
 
 
@@ -38,6 +38,47 @@ class IgnitionFileWriter(bpy.types.Operator, ExportHelper):
             if node.type == "BACKGROUND":
                 blendJson["Renderer"]["hdriMultiplier"] = node.inputs[1].default_value
 
+        # CAMERA SETTINGS
+        blendJson["Camera"] = {}
+        blendJson["Camera"]["fov"] = bpy.data.cameras[bpy.data.scenes[0].camera.name].angle
+        blendJson["Camera"]["pos"] = [bpy.data.scenes[0].camera.location[0],
+                                     -bpy.data.scenes[0].camera.location[2],
+                                      bpy.data.scenes[0].camera.location[1]]
+        
+        # oh boy here we go
+        # ok so to get the point at which the camera
+        # is looking at, I have to do 5 things;
+        # 1. Create new empty
+        # 2. Parent empty to camera (without inverse)
+        # 3. Move empty -1 on Z axis
+        # 4. Unparent empty and keep transform
+        # 5. Get empty location, that's the point the camera is loooking at.
+        # awfully complicated.
+
+        # step 1
+        objName = ' '.join([chr(random.randint(0,255)) for x in range(10)])
+        print(objName)
+        bpy.ops.object.empty_add(type='PLAIN_AXES') # empty is now selected
+        bpy.context.object.name = objName # randomly generated name for later
+        # step 2
+        bpy.context.object.select_set(False)
+        bpy.data.objects[objName].select_set(True)
+        bpy.data.scenes[0].camera.select_set(True)
+
+        bpy.ops.object.parent_no_inverse_set() # empty object now is parented to camera
+        for obj in bpy.context.selected_objects:
+            if obj.name != objName: # that's why we needed a random name
+                obj.select_set(False)
+        
+        # Step 3
+        bpy.context.object.location[2] -= 1
+
+        # Step 4
+        bpy.ops.object.parent_clear(type='CLEAR_KEEP_TRANSFORM')
+
+
+
+        
 
         # JSON -> IGNITION
         ignitionFile = ""
